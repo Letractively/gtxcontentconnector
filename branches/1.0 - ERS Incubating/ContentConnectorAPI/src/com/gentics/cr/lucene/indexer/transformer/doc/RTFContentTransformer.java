2c90596d032420258ae12e9aac849dee86d04ae6
supnig@constantinopel.at <supnig@constantinopel.at@a056d97c-4a9c-11de-8001-7f2440edb24e>
2009-11-03 15:13:07 +0000

    ADDED RTFContentTransformer ADDED FileExtensionCreator

--- branches/1.0 - ERS Incubating/ContentConnectorAPI/src/com/gentics/cr/lucene/indexer/transformer/doc/RTFContentTransformer.java
+++ branches/1.0 - ERS Incubating/ContentConnectorAPI/src/com/gentics/cr/lucene/indexer/transformer/doc/RTFContentTransformer.java
@@ -3,7 +3,10 @@ package com.gentics.cr.lucene.indexer.transformer.doc;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import org.apache.poi.hwpf.extractor.WordExtractor;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Document;
+import javax.swing.text.rtf.RTFEditorKit;
 
 import com.gentics.cr.CRException;
 import com.gentics.cr.CRResolvableBean;
@@ -16,14 +19,14 @@ import com.gentics.cr.lucene.indexer.transformer.ContentTransformer;
  * @author $Author$
  *
  */
-public class DOCContentTransformer extends ContentTransformer{
+public class RTFContentTransformer extends ContentTransformer{
 	private static final String TRANSFORMER_ATTRIBUTE_KEY="attribute";
 	private String attribute="";
 	/**
 	 * Get new instance of DOCContentTransformer
 	 * @param config
 	 */
-	public DOCContentTransformer(GenericConfiguration config)
+	public RTFContentTransformer(GenericConfiguration config)
 	{
 		super(config);
 		attribute = (String)config.get(TRANSFORMER_ATTRIBUTE_KEY);
@@ -46,15 +49,28 @@ public class DOCContentTransformer extends ContentTransformer{
 			throw new IllegalArgumentException("Parameter must be instance of byte[]");
 		}
 		String ret=null;
-		WordExtractor docextractor;
+		//creating a default blank styled document
+		 DefaultStyledDocument styledDoc = new DefaultStyledDocument();
+
+		 //Creating a RTF Editor kit
+		 RTFEditorKit rtfKit = new RTFEditorKit();
+
+		//Populating the contents in the blank styled document
 		try {
-			docextractor = new WordExtractor(is);
+				rtfKit.read(is,styledDoc,0);
 			
-			ret = docextractor.getText();
-			
+	
+			 // Getting the root document
+			 Document doc = styledDoc.getDefaultRootElement().getDocument();
+	
+			 //Printing out the contents of the RTF document as plain text
+			 ret = doc.getText(0,doc.getLength());
 		} catch (IOException e) {
 			throw new CRException(e);
-		} 
+		} catch (BadLocationException e) {
+			throw new CRException(e);
+		}
+
 		return(ret);
 	}
 	
