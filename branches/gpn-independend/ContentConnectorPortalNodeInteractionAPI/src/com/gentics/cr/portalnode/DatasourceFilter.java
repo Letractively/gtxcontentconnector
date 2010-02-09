017949a0cf6056821e2a0f5b9431d87aa96c2fc8
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-02-09 22:12:34 +0000

    added wrappers for the DatasourceFilter and the Datasource itself

---
+++ branches/gpn-independend/ContentConnectorPortalNodeInteractionAPI/src/com/gentics/cr/portalnode/DatasourceFilter.java
@@ -0,0 +1,11 @@
+package com.gentics.cr.portalnode;
+
+/**
+ * Interface to wrap the DatasourceFilter from Gentics Portal.Node.
+ * @author bigbear3001
+ *
+ */
+public interface DatasourceFilter extends
+    com.gentics.api.lib.expressionparser.filtergenerator.DatasourceFilter {
+
+}
