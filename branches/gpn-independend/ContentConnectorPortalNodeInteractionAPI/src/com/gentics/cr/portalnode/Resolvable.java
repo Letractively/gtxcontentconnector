8d63f7e9237d30b424ce9302c0f2daa5ad5cc6d7
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-02-09 22:13:22 +0000

    added a Resolvable interface for wrapping Genitcs Portal.Node Resolvables

---
+++ branches/gpn-independend/ContentConnectorPortalNodeInteractionAPI/src/com/gentics/cr/portalnode/Resolvable.java
@@ -0,0 +1,10 @@
+package com.gentics.cr.portalnode;
+
+/**
+ * Interface to wrap resovlables from Gentics Portal.Node
+ * @author bigbear3001
+ *
+ */
+public interface Resolvable extends com.gentics.api.lib.resolving.Resolvable {
+
+}
