1c9cd1f598f874a8d44a11c90bba44918e8ff2ac
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-02-09 22:21:04 +0000

    added package info (JAVA5)

---
+++ ContentConnectorPortalNodeInteractionAPI/src/com/gentics/cr/portalnode/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This is the main package containing wrappers and helpers to access Gentics
+ * Portal.Node objects and methods without bringing too much dependencies to the
+ * Gentics ContentConnector.
+ *
+ * Mainly because the Gentics Portal.Node API is not accessible to all people.
+ **/
+package com.gentics.cr.portalnode;
