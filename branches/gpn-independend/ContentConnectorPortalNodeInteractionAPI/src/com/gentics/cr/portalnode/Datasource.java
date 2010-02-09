16682d94999548e96dcdbcc1cbf9c7e9389208c5
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-02-09 20:29:28 +0000

    added a "wrapper" interface for datasources from Gentics Portal.Node

---
+++ branches/gpn-independend/ContentConnectorPortalNodeInteractionAPI/src/com/gentics/cr/portalnode/Datasource.java
@@ -0,0 +1,10 @@
+package com.gentics.cr.portalnode;
+
+/**
+ * Interface to wrap datasources from Gentics Portal.Node.
+ * @author bigbear3001
+ *
+ */
+public interface Datasource extends com.gentics.api.lib.datasource.Datasource {
+
+}
