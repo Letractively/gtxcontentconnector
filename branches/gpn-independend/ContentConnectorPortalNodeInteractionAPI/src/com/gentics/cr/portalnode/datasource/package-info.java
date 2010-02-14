711bbe9b94afb3dfbc5b7343abe66bd73c0351d2
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-02-14 10:53:49 +0000

    added java 5 package description

---
+++ branches/gpn-independend/ContentConnectorPortalNodeInteractionAPI/src/com/gentics/cr/portalnode/datasource/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package is intended to hold all Datasource specific Wrappers.
+ *
+ * Mainly because the Gentics Portal.Node API is not accessible to all people.
+ **/
+package com.gentics.cr.portalnode.datasource;
