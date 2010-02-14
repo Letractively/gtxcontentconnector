eac2fc2b36039bd21aeb294241ae712bef74ff0b
bigbear.ap <bigbear.ap@a056d97c-4a9c-11de-8001-7f2440edb24e>
2010-02-14 11:14:57 +0000

    added ResolvableBean to the Wrapping Classes (it is used for path bean)

---
+++ branches/gpn-independend/ContentConnectorPortalNodeInteractionAPI/src/com/gentics/cr/portalnode/ResolvableBean.java
@@ -0,0 +1,16 @@
+package com.gentics.cr.portalnode;
+
+/**
+ * Wrapper class for ResolvableBean.
+ * @author bigbear3001
+ *
+ */
+public class ResolvableBean extends
+    com.gentics.api.lib.resolving.ResolvableBean implements Resolvable {
+
+  /**
+   * generated serialVersionUID.
+   */
+  private static final long serialVersionUID = -229935052452636300L;
+
+}
