eef9fdca643f89af7313b8a2735dabb9da3705b5
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2011-05-30 07:40:36 +0000

    Added EvalExpressionParserTransformer (used to evaluate Expressions and put the result into the targetattribute)

--- branches/1.5.3/contentconnector-lucene/src/main/java/com/gentics/cr/lucene/indexer/transformer/EvalExpressionParserTransformer.java
+++ branches/1.5.3/contentconnector-lucene/src/main/java/com/gentics/cr/lucene/indexer/transformer/EvalExpressionParserTransformer.java
@@ -3,7 +3,18 @@ package com.gentics.cr.lucene.indexer.transformer;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+
+import com.gentics.api.lib.exception.ParserException;
+import com.gentics.api.lib.expressionparser.EvaluableExpression;
+import com.gentics.api.lib.expressionparser.ExpressionEvaluator;
+import com.gentics.api.lib.expressionparser.ExpressionParser;
+import com.gentics.api.lib.expressionparser.ExpressionParserException;
+import com.gentics.api.lib.expressionparser.ExpressionQueryRequest;
+import com.gentics.api.lib.resolving.PropertyResolver;
+import com.gentics.api.lib.resolving.Resolvable;
 import com.gentics.cr.CRResolvableBean;
 import com.gentics.cr.configuration.GenericConfiguration;
 import com.gentics.cr.util.CRUtil;
@@ -18,39 +29,58 @@ import com.gentics.cr.util.CRUtil;
 public class EvalExpressionParserTransformer extends ContentTransformer {
 	private static final String SRC_ATTRIBUTE_KEY = "srcattribute";
 	private static final String TARGET_ATTRIBUTE_KEY = "targetattribute";
-	private String src_attribute = "";
-	private String target_attribute = "ul";
+	private String providedExpression = "";
+	private String evaluatedTarget = "expressionresult";
 
+	private static Logger LOGGER = Logger.getLogger(EvalExpressionParserTransformer.class);
+	
 	/**
 	 * Create Instance of EvalExpressionParserTransformer.
 	 * 
-	 * @param config Configuration to read the attributes from.
+	 * @param config
 	 */
 	public EvalExpressionParserTransformer(final GenericConfiguration config) {
 		super(config);
-		src_attribute = (String) config.get(SRC_ATTRIBUTE_KEY);
-		target_attribute = (String) config.get(TARGET_ATTRIBUTE_KEY);
+		providedExpression = (String) config.get(SRC_ATTRIBUTE_KEY);
+		evaluatedTarget = (String) config.get(TARGET_ATTRIBUTE_KEY);
 	}
 
 	@Override
-	public void processBean(CRResolvableBean bean) {
-
-	}
-
-	private String getStringContents(Object obj) {
-		String str = null;
-		if (obj instanceof String) {
-			str = (String) obj;
-		} else if (obj instanceof byte[]) {
-			try {
-				str = CRUtil.readerToString(new InputStreamReader(new ByteArrayInputStream((byte[]) obj)));
-			} catch (IOException e) {
-				e.printStackTrace();
+	public void processBean(final CRResolvableBean bean) {
+		if (providedExpression != null) {
+			String evaluatedExpression = evaluateString(providedExpression, bean);
+			if (evaluatedExpression != null) {
+				bean.set(evaluatedTarget, evaluatedExpression);
 			}
 		}
-		return str;
 	}
 
+	/**
+	 * Used to evaluate an expression like concat("/Content.Node/", kategorie, filename) using ExpressionParser.
+	 * @param expressionString String to evaluate
+	 * @param resolvable Resolvable used to resolve properties (e.g. filename)
+	 * @return Result as string
+	 */
+	@SuppressWarnings("unchecked")
+	private static String evaluateString(final String expressionString, final Resolvable resolvable){
+	    String result = null;
+	    ExpressionParser expressionParser = ExpressionParser.getInstance();
+	    try {
+	            EvaluableExpression expression = (EvaluableExpression) expressionParser.parse(expressionString);
+	            ExpressionQueryRequest expressionQueryRequest = 
+	            	new ExpressionQueryRequest(new PropertyResolver(resolvable), new HashMap<Object, Object>(0));
+	            result = 
+	            	(String) expression.evaluate(expressionQueryRequest, ExpressionEvaluator.OBJECTTYPE_STRING);
+	            
+	    } catch (ExpressionParserException e) {
+	            LOGGER.error("Error while evaluating the expression (" 
+	            		+ expressionString + ") with the base resolvable (" + resolvable + ")", e);
+	    } catch (ParserException e) {
+	            LOGGER.error("Error parsing the expression (" + expressionString + ").", e);
+	    }
+	    return result;
+	}
+	
 	@Override
 	public void destroy() {
 
