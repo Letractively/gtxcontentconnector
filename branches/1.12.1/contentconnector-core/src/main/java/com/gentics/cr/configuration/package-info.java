05543db3b5271067f6a9cca8f467c782667de74a
friesoft@gmail.com <friesoft@gmail.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2013-02-15 08:55:49 +0000

    [maven-release-plugin]  copy for tag 1.12.1

---
+++ branches/1.12.1/contentconnector-core/src/main/java/com/gentics/cr/configuration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing configuration classes.
+**/
+package com.gentics.cr.configuration;
+
---
+++ branches/1.12.1/contentconnector-lucene/src/main/java/org/apache/lucene/search/spell/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package for lucene implementation of the spell checker.
+**/
+package org.apache.lucene.search.spell;
+
