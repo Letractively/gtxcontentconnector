118394e5eb8df3d6249dc3526dda299e0f58bc41
christopher@supnig.com <christopher@supnig.com@a056d97c-4a9c-11de-8001-7f2440edb24e>
2013-03-04 10:28:08 +0000

    [maven-release-plugin]  copy for tag 1.12.4

---
+++ branches/1.12.4/contentconnector-core/src/main/java/com/gentics/api/portalnode/connector/CCPLinkInformation.java
@@ -0,0 +1,9 @@
+package com.gentics.api.portalnode.connector;
+
+public class CCPLinkInformation extends PLinkInformation {
+
+	public CCPLinkInformation(String contentId) {
+		super(contentId, null);
+	}
+
+}
